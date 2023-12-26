3e6b76955fd476aa62d26e7c1a9a1199edf572d3
Graham Williams <graham.williams@togaware.com>
2023-12-27 08:16:22 +1100

    Add window manager to support window on top and sizing.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,74 @@
+/// This is a basic template app to begin a Solid POD project.
+//
+// Time-stamp: <Wednesday 2023-12-27 08:10:44 +1100 Graham Williams>
+//
+/// Copyright (C) 2024, Graham.Williams@togaware.com
+///
+/// Licensed under the GNU General Public License, Version 3 (the "License");
+///
+/// License: https://www.gnu.org/licenses/gpl-3.0.en.html
+//
+// This program is free software: you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free Software
+// Foundation, either version 3 of the License, or (at your option) any later
+// version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+// details.
+//
+// You should have received a copy of the GNU General Public License along with
+// this program.  If not, see <https://www.gnu.org/licenses/>.
+///
+/// Authors: Graham Williams
+
 import 'package:flutter/material.dart';
 
-void main() {
+import 'package:solid/solid.dart';
+import 'package:window_manager/window_manager.dart';
+
+import 'utils/is_desktop.dart';
+
+void main() async {
+  // Remove [debugPrint] messages from production code.
+
+  debugPrint = (String? message, {int? wrapWidth}) {
+    null;
+  };
+
+  if (isDesktop) {
+    WidgetsFlutterBinding.ensureInitialized();
+
+    await windowManager.ensureInitialized();
+
+    WindowOptions windowOptions = const WindowOptions(
+      // Setting [alwaysOnTop] here will ensure the app starts on top of other
+      // apps on the desktop so that it is visible. We later turn it of as we
+      // don't want to force it always on top.
+
+      alwaysOnTop: true,
+
+      // The size is overridden in the first instance by linux/my_application.cc
+      // but setting it here then does have effect when Restarting the app.
+
+      // Windows has 1280x720 by default in windows/runner/main.cpp line 29 so
+      // best not to override it here since under windows the 950x600 is too
+      // small.
+
+      //size: Size(750, 873),
+
+      // The [title] is used for the window manager's window title.
+
+      title: 'PodNotes - A note taking app with private PODs',
+    );
+
+    windowManager.waitUntilReadyToShow(windowOptions, () async {
+      await windowManager.show();
+      await windowManager.focus();
+      await windowManager.setAlwaysOnTop(false);
+    });
+  }
   runApp(const MyApp());
 }
 
@@ -31,10 +99,17 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home:
+          const SolidLogin(child: MyHomePage(title: 'Flutter Demo Home Page')),
     );
   }
 }
+
+/// The default sample [Widget] for flutter.
+///
+/// This is retained for now only as a demonstration. It will be replaced with
+/// the [Widget] to display, and the the add/edit entries within the key value
+/// pair store.
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
